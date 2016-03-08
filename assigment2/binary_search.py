37c1b64dff91597a7d58450b8090aa7207cd0a70
Özcan Esen <ozcanesen@gmail.com>
2016-03-08 20:22:11 +0200

    small fixes

--- assigment2/binary_search.py
+++ assigment2/binary_search.py
@@ -2,20 +2,21 @@ import random
 
 def binarySearch(liste, aranan):
 	baslangic = 0
-	bitis = len(liste)
+	bitis = len(liste) - 1
 	denemeSayisi = 0
-	while (baslangic != bitis):
+	while (baslangic <= bitis):
 		denemeSayisi = denemeSayisi + 1
 
-		orta = int((baslangic + bitis) / 2)
+		orta = (baslangic + bitis) // 2
 		bulunan = liste[orta]
 
 		if (bulunan == aranan):
 			return denemeSayisi
 		elif (bulunan > aranan):
-			bitis = orta
+			bitis = orta - 1
 		else:
-			baslangic = orta
+			baslangic = orta + 1
+	return -1 # bulunamadi
 
 diziBoyutlari = [1,10,100,1000,10000,100000,1000000,10000000,100000000]
 
@@ -26,7 +27,8 @@ for diziBoyutu in diziBoyutlari:
 	toplamDenemeSayisi = 0
 
 	for i in range(kacKereDenenecek):
-		aranan = random.randint(0, diziBoyutu - 1)
+		#aranan = random.randint(0, diziBoyutu - 1)
+		aranan = -73
 		denemeSayisi = binarySearch(dizi, aranan)
 		toplamDenemeSayisi = toplamDenemeSayisi + denemeSayisi
 
